44db610e19eb50f117a724214d45a33f27bf7946
Kolibri <klevis@resulaj.eu>
2023-01-06 23:45:01 +0100

    final reworks currently beta mode

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,6 @@
-
- fetch("/file-available")
-  .then(response => response.json()) // Parse the response as JSON
-  .then(data => {
+fetch("/file-available")
+  .then((response) => response.json()) // Parse the response as JSON
+  .then((data) => {
     // Check the value of the "available" property in the response data
     if (data.available) {
       // If the file is available, show the download button
@@ -11,29 +10,22 @@
       document.getElementById("download-button").style.display = "none";
     }
   })
-   .catch(error => {
-    
+  .catch((error) => {
     console.log(error);
-
-});
-
-
-// Update the text of the error message element
-document.getElementById("error-message").innerText = "Please select at least two PDF files to merge.";
-
-
-  // Define a function to check the file availability
-  function checkFileAvailability() {
-    // Make an HTTP GET request to the /file-available route on your server
-    fetch("/file-available")
-      .then(response => response.json()) // Parse the response as JSON
-      .then(data => {
-        // Check the value of the "available" property in the response data
-        if (data.available) {
-          // If the file is available, show the download button
-          document.getElementById("download-button").style.display = "block";
-        } else {
-          // If the file is not available, hide the download button
-          document.getElementById("download-button").style.display = "none";
-        }
-      })};
+  });
+// Define a function to check the file availability
+function checkFileAvailability() {
+  // Make an HTTP GET request to the /file-available route on your server
+  fetch("/file-available")
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => {
+      // Check the value of the "available" property in the response data
+      if (data.available) {
+        // If the file is available, show the download button
+        document.getElementById("download-button").style.display = "block";
+      } else {
+        // If the file is not available, hide the download button
+        document.getElementById("download-button").style.display = "none";
+      }
+    });
+}
