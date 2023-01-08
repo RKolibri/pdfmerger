311ad48407a2d11a2746f6dc2d630c1241482c59
Kolibri <klevis@resulaj.eu>
2023-01-08 01:16:53 +0100

    Session id on url fixed

--- src/main/java/eu/resulaj/pdf/WebConfig.java
+++ src/main/java/eu/resulaj/pdf/WebConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer 
+{
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
+
+
 }
